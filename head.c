ac573d8b90e3c0bb74a72d771eeda18979f672b8
Masaki Kitazawa <redcrow.urawa@gmail.com>
2020-07-23 21:14:19 +0900

    update

--- head.c
+++ head.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+/* (1) */
+#define _GNU_SOURCE
+#include <getopt.h>
 
 static void do_head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+/* (2) */
+static struct option longopts[] = {
+    {"lines", required_argument, NULL, 'n'},
+    {"help",  no_argument,       NULL, 'h'},
+    {0, 0, 0, 0}
+};
 
 int
 main(int argc, char *argv[])
 {
-    long nlines;
+    int opt;
+    long nlines = DEFAULT_N_LINES;
 
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
-        exit(1);
+    /* (3) */
+    while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+        switch (opt) {
+        case 'n':         /* (4) */
+            nlines = atol(optarg);
+            break;
+        case 'h':         /* (5) */
+            fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+            exit(0);
+        case '?':         /* (6) */
+            fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+            exit(1);
+        }
     }
-    nlines = atol(argv[1]);
-    if (argc == 2) {
+    /* (7) */
+    if (optind == argc) {
         do_head(stdin, nlines);
-    } else {    /* この節が追加された */
+    } else {
         int i;
 
-        for (i = 2; i < argc; i++) {
+        /* (7') */
+        for (i = optind; i < argc; i++) {
             FILE *f;
 
             f = fopen(argv[i], "r");
